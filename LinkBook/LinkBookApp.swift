30e91b42ad3629a3045a3038e50905f9fc6d7ce8
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-03-17 16:05:56 +0900

    Initial Commit

---
+++ LinkBook/LinkBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkBookApp.swift
+//  LinkBook
+//
+//  Created by Masahiko Kobayashi on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct LinkBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
