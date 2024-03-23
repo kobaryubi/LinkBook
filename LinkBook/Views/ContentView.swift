69f4fe11856aec0df64137d8f413615bac685e77
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-03-23 17:07:16 +0900

    feat: add navigation stack

--- LinkBook/Views/ContentView.swift
+++ LinkBook/Views/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
 
