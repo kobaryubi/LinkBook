e8b3ad652d9abe3c7db9409a487d2a2ed68f0b1f
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-03-25 21:24:08 +0900

    feat: add link struct

---
+++ LinkBook/Models/Link.swift
@@ -0,0 +1,13 @@
+//
+//  Link.swift
+//  LinkBook
+//
+//  Created by Masahiko Kobayashi on 2024/03/25.
+//
+
+import Foundation
+
+struct Link: Identifiable {
+    var id: String
+    var url: String
+}
